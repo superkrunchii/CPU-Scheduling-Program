fb1ec4d1db4d6c72aea90b87e037b588eb66c262
Jerickson <jerickson_lee@dlsu.edu.ph>
2022-05-12 16:45:54 +0800

    Added File Inputs

--- main.c
+++ main.c
@@ -1,10 +1,50 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void RoundRobin() {
+
+}
+
+void SRTF() {
+
+}
+
 
 int main() {
-    int valid = 0;
+
+    char filename[50];
+    char test[2];
+    int x, y, z;
+    int c;
+
     printf("Enter filename: ");
-    while (valid != 1) {
-        fgets()
+
+    scanf("%s" , filename);
+    FILE *f = fopen(strcat(filename , ".txt") , "r");
+
+    if(f == NULL) {
+        perror("File does not exist");
+
+    } else {
+
+        while ((c = fgetc(f)) != EOF){
+            fgets(test, 2, f); // X
+            x = atoi(test);
+
+            fgets(test, 3, f); // Y
+            y = atoi(test);
+
+            fgets(test, 3, f); // Z
+            z = atoi(test);
+
+            printf("INPUTS: %d %d %d \n", x, y, z);
+        }
+
     }
+    
+    fclose(f);
+    
+
     return 0;
 }

855425023db69e58a5d221fb4d16e089debd0eff
Jerickson <jerickson_lee@dlsu.edu.ph>
2022-05-13 17:09:10 +0800

    Added New Inputs

--- main.c
+++ main.c
@@ -15,8 +15,8 @@ int main() {
 
     char filename[50];
     char test[2];
-    int x, y, z;
-    int c;
+    int settings[2];
+    int i;
 
     printf("Enter filename: ");
 
@@ -28,18 +28,43 @@ int main() {
 
     } else {
 
-        while ((c = fgetc(f)) != EOF){
-            fgets(test, 2, f); // X
-            x = atoi(test);
+        fgets(test, 2, f); // X (Algorithm)
+        settings[0] = atoi(test);
 
-            fgets(test, 3, f); // Y
-            y = atoi(test);
+        fgets(test, 3, f); // Y (Number of Process)
+        settings[1] = atoi(test);
 
-            fgets(test, 3, f); // Z
-            z = atoi(test);
+        fgets(test, 3, f); // Z (Time Slice) Set to 1 If not RR
+        settings[2] = atoi(test);
 
-            printf("INPUTS: %d %d %d \n", x, y, z);
+        printf("%d %d %d\n", settings[0], settings[1], settings[2]);
+
+        int tasks[settings[1]][2];
+
+        fgets(test, 3, f); // Remove Extra Spaces
+
+        for(i = 0; i < settings[1]; i++) {
+
+            fgets(test, 2, f);
+
+            tasks[i][0] = atoi(test);
+
+            fgets(test, 3, f);
+
+            tasks[i][1] = atoi(test);
+
+            fgets(test, 3, f);
+
+            tasks[i][2] = atoi(test);
+
+            fgets(test, 3, f); // Remove Extra Spaces
+
+            printf("%d %d %d\n" , tasks[i][0] , tasks[i][1] , tasks[i][2]);
+
         }
+
+        
+        
 
     }
     
